f6780543feebd71a126cf654752754ffbebf972c
jackcat13 <christophehenry11@gmail.com>
2023-08-08 20:50:37 +0200

    feat(rendering): printing of entities with centering of camera on one entity

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,80 @@
-use std::{thread::sleep, time};
+use std::{io, time::Duration};
 
+use crossterm::{
+    event::{Event, EventStream, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+use futures::{FutureExt, StreamExt};
+use futures_timer::Delay;
 use rendering::{
-    entity::{print_sprites, Entity},
+    entity::{print_sprites_centered_on, Entity},
     position::Position,
     sprite::load_sprite,
 };
+use tokio::select;
 
 mod rendering;
 
+const TERM_SIZE_X: u16 = 20_000;
+const TERM_SIZE_Y: u16 = 20_000;
+
 //Main only useful to do some manual tests in the library
-fn main() {
-    let mut entities = vec![
-        Entity {
-            name: "bob".to_string(),
-            sprite: load_sprite("./manualTests/simple_sprite.json".to_string()),
-            position: Position { x: 50, y: 0 },
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let _ = crossterm::terminal::SetSize(TERM_SIZE_X, TERM_SIZE_Y);
+    enable_raw_mode().expect("Failed enabling raw mode");
+    game().await;
+    disable_raw_mode()
+}
+
+async fn game() {
+    let mut bob = Entity {
+        name: "bob".to_string(),
+        sprite: load_sprite("./manualTests/bob.json".to_string()),
+        position: Position {
+            x: TERM_SIZE_X / 2,
+            y: TERM_SIZE_Y / 2,
         },
-        Entity {
-            name: "bob2".to_string(),
-            sprite: load_sprite("./manualTests/simple_sprite.json".to_string()),
-            position: Position { x: 100, y: 10 },
+    };
+    let enemies = vec![Entity {
+        name: "enemy".to_string(),
+        sprite: load_sprite("./manualTests/enemy.json".to_string()),
+        position: Position {
+            x: (TERM_SIZE_X / 2) + 20,
+            y: (TERM_SIZE_Y / 2) + 20,
         },
-    ];
+    }];
+    let mut reader = EventStream::new();
     loop {
-        print_sprites(&entities);
+        print_sprites_centered_on(&bob, &enemies);
 
-        entities.get_mut(0).unwrap().position.x += 1;
-
-        sleep(time::Duration::from_millis(10));
+        let delay = Delay::new(Duration::from_millis(1_000)).fuse();
+        let event = reader.next().fuse();
+        select! {
+            _ = delay => { println!(".\r"); },
+            maybe_event = event => {
+                match maybe_event {
+                    Some(Ok(event)) => {
+                        if event == Event::Key(KeyCode::Char('z').into()) {
+                            bob.position.y -= 1;
+                        }
+                        if event == Event::Key(KeyCode::Char('s').into()) {
+                            bob.position.y += 1;
+                        }
+                        if event == Event::Key(KeyCode::Char('q').into()) {
+                            bob.position.x -= 1;
+                        }
+                        if event == Event::Key(KeyCode::Char('d').into()) {
+                            bob.position.x += 1;
+                        }
+                        if event == Event::Key(KeyCode::Esc.into()) {
+                            break;
+                        }
+                    }
+                    Some(Err(_)) => {},
+                    None => {},
+                }
+            }
+        };
     }
 }
--- src/rendering/entity.rs
+++ src/rendering/entity.rs
@@ -1,6 +1,14 @@
-use std::io::{stdout, Write};
+use std::{
+    io::{stdout, Write},
+    panic,
+};
 
-use crossterm::{cursor::MoveTo, style::Print, terminal, QueueableCommand};
+use crossterm::{
+    cursor::MoveTo,
+    style::Print,
+    terminal::{self, size},
+    QueueableCommand,
+};
 
 use super::{position::Position, sprite::Sprite};
 
@@ -38,4 +46,35 @@ fn print_sprite(sprite: &Sprite, original_x: u16, original_y: u16) {
         x = original_x;
         y += 1;
     });
+}
+
+#[allow(dead_code)]
+pub fn print_sprites_centered_on(entity_centered: &Entity, other_entities: &[Entity]) {
+    let mut stdout = stdout();
+    let (term_size_x, term_size_y) = size().expect("Failed to get terminal size");
+    let middle_x = term_size_x / 2;
+    let middle_y = term_size_y / 2;
+    stdout
+        .queue(terminal::Clear(terminal::ClearType::All))
+        .expect("Failed to clean terminal");
+    print_sprite(&entity_centered.sprite, middle_x, middle_y);
+    other_entities.iter().for_each(|entity| {
+        let old_hook = panic::take_hook();
+        panic::set_hook(Box::new(|_info| {}));
+        let maybe_error = std::panic::catch_unwind(|| {
+            print_sprite(
+                &entity.sprite,
+                middle_x + entity.position.x - entity_centered.position.x,
+                middle_y + entity.position.y - entity_centered.position.y,
+            )
+        });
+        if maybe_error.is_err() {}
+        panic::set_hook(old_hook);
+    });
+    stdout
+        .queue(MoveTo(term_size_x, term_size_y))
+        .expect("Failed to move to last position")
+        .queue(Print(" "))
+        .expect("Failed to print to last position");
+    stdout.flush().expect("Failed to flush terminal prints");
 }
